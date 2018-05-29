cd22a04c1445d92bae0ef5ae0c38d0b65997bce5
Tai Hovanky <tai@Tais-MacBook-Pro.local>
2018-05-29 13:37:41 -0500

    starting-app-nav

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+import AuthScreen from './screens/AuthScreen';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+const store = configureStore();
+
+Navigation.registerComponent(
+  'arrive-demo.AuthScreen',
+  () => AuthScreen,
+  store,
+  Provider
+);
+
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: 'arrive-demo.AuthScreen',
+    title: 'Login'
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+})
