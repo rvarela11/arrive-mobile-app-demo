89c69a3085039d6e1dc3bb6e4261ed733cbfc6d1
Roberto  Varela <rvarela@Robertos-MacBook-Pro.local>
2018-06-12 11:26:43 -0500

    Added global Toast component

---
+++ src/components/UI/DefaultToast/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+import styleVariables from '../../../style-variables';
+
+const styles = StyleSheet.create({
+  toast: {
+    width: '35%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 4,
+    borderRadius: 2,
+  },
+  toast__text: {
+    color: '#fff'
+  },
+  toastSuccess: {
+    backgroundColor: styleVariables.green
+  },
+  toastError: {
+    backgroundColor: styleVariables.red
+  }
+});
+
+export default styles;
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,5 +1,5 @@
 // Vendors
-import React from 'react';
+import React,{ Component } from 'react';
 import {
     View,
     Text,
@@ -8,19 +8,52 @@ import {
 import { Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 
-const HomeScreen = props => (
-    <View>
-        {props.homeDocuments.map((document) => {
-            return (
-                <Card key={document.title}>
-                    <Text>{document.title}</Text>
-                    <Text>{document.docType}</Text>
-                    <Text>{document.notes}</Text>
-                </Card>
-            );
-        })}
-    </View>
-);
+// Components
+import DefaultToast from '../../components/UI/DefaultToast';
+
+// Styling
+import styles from './styles';
+
+class HomeScreen extends Component {
+
+  state = {
+    isToastSuccessful: true,
+    showToast: false,
+    homeDocuments: 0
+  }
+
+  showToast = () => {
+    if(this.props.homeDocuments.length === this.state.homeDocuments) {
+      return 'Upload Error';
+
+    }
+    return 'Upload Successful';
+
+  }
+
+  render () {
+    const { isToastSuccessful } = this.state;
+    return (
+        <View>
+          <View style={styles.toastContainer}>
+            <DefaultToast
+              showToast={showToast}
+              valid={isToastSuccessful}
+            >{this.showToast()}</DefaultToast>
+          </View>
+            {this.props.homeDocuments.map((document) => {
+                return (
+                    <Card key={document.title}>
+                        <Text>{document.title}</Text>
+                        <Text>{document.docType}</Text>
+                        <Text>{document.notes}</Text>
+                    </Card>
+                );
+            })}
+        </View>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
     homeDocuments: state.homeDocuments
